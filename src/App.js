6ec82e8bf00b618b76ba3720d884771cce40a9d0
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2024-04-13 15:53:56 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import PhoneMockup from "./assets/mock2.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        // className="w-fit max-h-[330px] md:max-h-[450px] lg:max-h-[450px]"
+        src={PhoneMockup}
+      />
     </div>
   );
 }
